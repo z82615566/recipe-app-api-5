5b4997a1527215e6f3d076d8f5d3f7daf95e673e
unknown <a22646185@gmail.com>
2022-06-16 20:17:21 +0800

    lint fix

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -6,13 +6,16 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('django.db.utils.ConnectionHandler.__getitem__')
 class CommandTests(SimpleTestCase):
+
     def test_wait_for_db_ready(self, patched_getitem):
         """Test waiting for database if database ready."""
         patched_getitem.return_value = True
         call_command('wait_for_db')
         self.assertEqual(patched_getitem.call_count, 1)
+
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_getitem):
         patched_getitem.side_effect = [Psycopg2OpError] + \
